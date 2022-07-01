cddf8e927f66270b291df720fc6d4b3ff19fdf2f
shifanafin <ctshifanachennara@gmail.com>
2022-07-01 18:10:26 +0530

    contact list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./Contact";
+import contacts from "./contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">My Contacts</h1>
+      <Contact  
+      name={contacts[0].name}
+      img={contacts[0].img}
+      tel={contacts[0].phone}
+      email={contacts[0].email}
+      />
+      <Contact  
+      name={contacts[1].name}
+      img={contacts[1].img}
+      tel={contacts[1].phone}
+      email={contacts[1].email}
+      />
+       <Contact  
+      name={contacts[0].name}
+      img={contacts[0].img}
+      tel={contacts[0].phone}
+      email={contacts[0].email}
+      />
+       <Contact  
+      name={contacts[1].name}
+      img={contacts[1].img}
+      tel={contacts[1].phone}
+      email={contacts[1].email}
+      />
     </div>
   );
 }
